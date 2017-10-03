28c21fbfce7529ff226c79a053c4e20bd634e312
Sivaraj Nagaraj <sivarajng@gmail.com>
2017-10-03 17:59:38 +0530

    initial components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { EditComponent } from './edit/edit.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent,
+    EditComponent,
+    FooterComponent,
+    HeaderComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule
